63e1ea54fe545789f63a2fcf0fc6350eecff1a48
Louis-MG <lm.gueguen@orange.fr>
2022-03-02 18:02:36 +0100

    added error when reading file, added comments for structure

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+#include <cerrno>
 
 int main(int argc, char** argv) {
     std::string matrix = argv[1];
     std::string output = argv[2];
-    std::ifstream infile;
-    infile.open(matrix, std::ios::binary)
-    //ofstream for output
-    //after openning the file, we must read it :
-    while(! infile.eof())
-    {
-        getline(infile,line[x]);                                           // Fill line array
-        x++;                                                          // count
-    }
-    return 0;
+    std::ifstream srteam; //does not work ?
+    stream.open(matrix, std::ios::binary);
     //must use infile.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
     // see https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/cppBinaryFileIO-2.html
+    //TODO: separate the funcitons in opening, reading, writing, and returning errors
+    if (stream.fail()) {
+        std::cout << "ERROR: could not open file:" << strerror(errno) << std::endl;
+        std::exit(1);
+    }
+    //after opening the file, we must read it :
+    while(! stream.eof())
+    {
+        //do smthg                                                        // count
+    }
+    return 0;
+    //ofstream for output
 }
