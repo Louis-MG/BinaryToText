a2a9a6516dd6027c2e21068deacb6915506268d3
Louis-MG <lm.gueguen@orange.fr>
2022-03-03 10:19:35 +0100

    resolved compoilation errors

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include <cerrno>
 
 int main(int argc, char** argv) {
     std::string matrix = argv[1];
     std::string output = argv[2];
-    std::ifstream srteam; //does not work ?
+    std::ifstream stream;
     stream.open(matrix, std::ios::binary);
-    //must use infile.fail() with a switch case to now if the thing went fine : no
+    //must use stream.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
     // see https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/cppBinaryFileIO-2.html
-    //TODO: separate the funcitons in opening, reading, writing, and returning errors
+    //TODO: find out why strerror is not recognised
     if (stream.fail()) {
-        std::cout << "ERROR: could not open file:" << strerror(errno) << std::endl;
+        perror("ERROR: could not open file:");
         std::exit(1);
     }
     //after opening the file, we must read it :
     while(! stream.eof())
     {
-        //do smthg                                                        // count
+        std::string line ;
+        while(getline(stream, line)) {
+            std::cout << line << std::endl;
+        }
     }
     return 0;
     //ofstream for output
