06721621281d240bb9ae6e81f0db26fdc752de0d
lmgueguen <lm.gueguen@orange.fr>
2022-03-04 11:52:23 +0100

    reordered, still errors

--- main.cpp
+++ main.cpp
@@ -3,29 +3,28 @@
 #include <cerrno>
 
 int main(int argc, char** argv) {
-    std::string matrix = argv[1];
-    std::string output = argv[2];
-    std::ifstream stream;
-    std::ofstream outstream;
+    std::string matrix;
+    matrix = argv[1];
+    //std::string output = argv[2];
+    std::ifstream stream (matrix, std::ifstream::binary);
+    //std::ofstream outstream;
 
-    // get size of file
-    stream.seekg (0,stream.end);
-    long size = stream.tellg();
-    stream.seekg (0);
 
-    // allocate memory for file content
-    char* buffer = new char[size];
-
-    stream.open(matrix, std::ios::binary);
     //must use stream.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
     // see https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/cppBinaryFileIO-2.html
-    //TODO: find out why strerror is not recognised
     if (stream.fail()) {
         perror("ERROR: could not open file:");
         std::exit(1);
     }
 
+    // get size of file
+    stream.seekg (0,stream.end);
+    long size = stream.tellg();
+    stream.seekg (0, stream.beg);
+
+    // allocate memory for file content
+    char* buffer = new char[size];
 
     // read data as a block:
     stream.read (buffer,size);
@@ -34,7 +33,7 @@ int main(int argc, char** argv) {
     std::cout.write (buffer,size);
     // release dynamically-allocated memory
     delete[] buffer;
-    outstream.close();
+    //outstream.close();
     stream.close();
 
     return 0;
