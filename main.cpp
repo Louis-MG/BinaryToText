0f2ffbe2c0e9685cc22b5226bb0f8318cc7a478c
Louis-MG <lm.gueguen@orange.fr>
2022-03-07 16:56:29 +0100

    Now displays smthg in the standard output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <cstring>
 
 void process_line(std::string &line);
 
@@ -8,12 +8,13 @@ int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "ERROR: invalid number of arguments" << std::endl;
     }
-    std::string line;
+    std::string();
     std::string matrix = argv[1];
     //std::string output = argv[2];
     std::ifstream stream (matrix, std::ifstream::binary);
     //std::ofstream outstream;
 
+    std::cout << "this is fine 1" << std::endl;
 
     //must use stream.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
@@ -27,23 +28,26 @@ int main(int argc, char* argv[]) {
     //stream.read (buffer,size);
 
     //read data by chunks
-    std::vector<char> buffer (4096,0); //reads only the first 4096 bytes
-    while(!stream.good()) { //dont use !stream.eof() because it wont reach
+    //std::vector<char> buffer (4096,0); //reads only the first 4096 bytes
+    char buffer[4096];
+    while(stream.good()) { //dont use !stream.eof() because it wont reach
         // the endOfFile as long as we did not actually read it until
         // the end
-        stream.read(buffer.data(), buffer.size());
+        stream.read(buffer, sizeof buffer);
         std::streamsize s=stream.gcount();
+        std::cout << s << std::endl;
         //read the data line by line:
-        while (std::getline(stream, line)) //cant use s because int
-        {
-            //std::istringstream iss(line);
-            process_line(line);
+        char *line = strtok(buffer, "\n");
+        while (line) {
+            std::string line_to_str;
+            line_to_str = line;
+            process_line(line_to_str);
+            line = strtok(buffer, "\n");
         }
-
     }
     //outstream.close();
     stream.close();
-
+    //delete[] buffer;
     return 0;
     //ofstream for output
 }
