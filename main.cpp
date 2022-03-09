9820005e955fcf650c0f45a8ed0336442c3515cb
Louis-Mael GUEGUEN <lm.gueguen@orange.fr>
2022-03-09 11:29:06 +0100

    added output argument, error check and writing

--- main.cpp
+++ main.cpp
@@ -11,31 +11,34 @@ int main(int argc, char* argv[]) {
     }
     std::string();
     std::string matrix = argv[1];
-    //std::string output = argv[2];
+    std::string output = argv[2];
     std::ifstream stream (matrix, std::ifstream::binary);
-    //std::ofstream outstream;
+    std::ofstream outstream (output, std::ofstream::binary);
 
     //must use stream.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
     // see https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/cppBinaryFileIO-2.html
     if (stream.fail()) {
-        perror("ERROR: could not open file:");
+        perror("ERROR: could not open input file");
+        std::exit(1);
+    } else if (outstream.fail()) {
+        perror("ERROR: could not open output file");
         std::exit(1);
     }
 
     //read data by chunks
-    while(stream.good()) { //dont use !stream.eof() because it wont reach
+    while(stream.good() and outstream.good()) { //dont use !stream.eof() because it wont reach
         // the endOfFile as long as we did not actually read it until
         //read the data line by line:
         std::string line_buffer;
         std::string variable;
         while(std::getline(stream, line_buffer).good()) {
             variable = process_line(line_buffer);
-            std::cout << variable << std::endl;
+            outstream << variable << std::endl;
         }
 
     }
-    //outstream.close();
+    outstream.close();
     stream.close();
     return 0;
     //ofstream for output
