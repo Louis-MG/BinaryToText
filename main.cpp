ef9271ff1e4fd0522812afdbd4a6434938a4c55e
Louis-MG <lm.gueguen@orange.fr>
2022-03-09 17:42:48 +0100

    added kmer struct, vectors as a workaround the line to columns problem

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 #include <string>
 #include <sstream>
 #include <chrono>
+#include <vector>
 
 std::string process_line(const std::string& line_buffer);
+
+struct kmer {
+    std::string name;
+    std::vector<int> pattern;
+};
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -21,6 +27,7 @@ int main(int argc, char* argv[]) {
     std::string output = argv[2]; //output presence/absence matrix
     std::ifstream stream (matrix, std::ifstream::binary);
     std::ofstream outstream (output, std::ofstream::binary);
+    std::vector<kmer> vectorOfKmers;
 
     //must use stream.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
@@ -41,13 +48,17 @@ int main(int argc, char* argv[]) {
         std::string line_buffer;
         std::string line;
         while(std::getline(stream, line_buffer).good()) {
-            line = process_line(line_buffer);
-            outstream << line << std::endl;
+            kmer kmer_buffer = process_line(line_buffer);
+            vectorOfKmers.push_back(kmer_buffer);
+            //outstream << line << std::endl;
         }
-
     }
+    stream.close()
+
+    //iterate with iterator on vectorOfKmer
+
     outstream.close();
-    stream.close();
+
     //finishing measuring time
     auto t2 = std::chrono::high_resolution_clock::now();
     auto ms_int = std::chrono::duration_cast<std::chrono::minutes>(t2 - t1);
@@ -56,18 +67,23 @@ int main(int argc, char* argv[]) {
 }
 
 std::string process_line(const std::string& line_buffer) {
-    std::string output_line;
+    /*
+     * this function processes lines by puting them in a structure than contains  its name, and a vector of its absence/presence pattern.
+     */
+    std::vector<int> output_pattern;
+    output_pattern.clear();
     std::istringstream input;
     input.str(line_buffer);
     //loop over tab-separated words in the line :
     for (std::string word; std::getline(input, word, '\t'); ) {
         if (word.starts_with(">")) { // if id resets line
-            output_line = word;
+            std::string kmer_name = word;
         } else if (word.ends_with("*")) { // if abundance is 0
-            output_line.append("\t0");
+            output_pattern.push_back(0);
         } else {
-            output_line.append("\t1"); // if abundance is anything else than 0
+            output_pattern.push_back(1); // if abundance is anything else than 0
         }
     }
-    return output_line;
+    kmer output_struct{kmer_name, output_pattern};
+    return output_struct;
 }
