602f3711888594d78c537b5e8d2d7461f152ff0d
Louis-MG <lm.gueguen@orange.fr>
2022-03-09 14:19:11 +0100

    change from milliseconds to seconds

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char* argv[]) {
     stream.close();
     //finishing measuring time
     auto t2 = std::chrono::high_resolution_clock::now();
-    auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    auto ms_int = std::chrono::duration_cast<std::chrono::seconds>(t2 - t1);
     std::cout << ms_int.count() << "ms\n";
     return 0;
 }
