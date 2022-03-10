7b193e167d671b7e890d641b139a508adb60d046
lmgueguen <lm.gueguen@orange.fr>
2022-03-10 09:57:36 +0100

    loop over columns fixed, output verified

--- main.cpp
+++ main.cpp
@@ -56,13 +56,15 @@ int main(int argc, char* argv[]) {
 
     //iterate with iterator on vectorOfKmer
     for (auto i : vectorOfKmers) {
-        outstream << i.name + "\t" ;
+        outstream << i.name << "\t" ;
     }
     outstream << "\n" ;
     for (int i = 0; i < vectorOfKmers.at(1).pattern.size(); ++i) { //gets the number of lines that will be written, which corresponds to the number of 0/1 in the vector pattern of the structures
+        std::cout << i << std::endl;
         //go through the ieme values of each vector
         for (auto j: vectorOfKmers) {
-            outstream << j.pattern.at(i) + "\t";
+            std::cout << j.pattern.at(i) << std::endl;
+            outstream << j.pattern.at(i) << "\t";
         }
         outstream << "\n" ;
     }
