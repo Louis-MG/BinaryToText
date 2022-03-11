0b039345f237e3e5cca4434e0ddb7ba060c09ffb
lmgueguen <lm.gueguen@orange.fr>
2022-03-11 12:00:55 +0100

    added srtucture to build unique_rows file

--- main.cpp
+++ main.cpp
@@ -28,6 +28,9 @@ int main(int argc, char* argv[]) {
     std::string output = argv[2]; //output presence/absence matrix
     std::ifstream stream (matrix, std::ifstream::binary);
     std::ofstream outstream (output, std::ofstream::binary);
+    size_t lastindex = output.find_last_of(".");
+    std::string rawname = output.substr(0, lastindex);
+    std::ofstream outstream_unique (rawname+"_unique.txt", std::ofstream::binary);
     // must use stream.fail() with a switch case to now if the thing went fine : no
     // file operation causes C++ to stop.
     // see https://www.eecs.umich.edu/courses/eecs380/HANDOUTS/cppBinaryFileIO-2.html
@@ -37,9 +40,12 @@ int main(int argc, char* argv[]) {
     } else if (outstream.fail()) {
         perror("ERROR: could not open output file");
         std::exit(1);
+    } else if (outstream_unique.fail()) {
+        perror("ERROR: could not open output_unique file");
+        std::exit(1);
     }
-    std::vector<Kmer> vectorOfKmers;
-
+    std::vector<Kmer> vector_of_kmers;
+    std::vector<std::vector<int>> vector_of_unique_patterns ;
 
     //read data by chunks
     while(stream.good() and outstream.good()) {
@@ -48,27 +54,45 @@ int main(int argc, char* argv[]) {
         // read the data line by line:
         std::string line_buffer;
         while(std::getline(stream, line_buffer).good()) {
-            vectorOfKmers.push_back(process_line(line_buffer));
+            vector_of_kmers.push_back(process_line(line_buffer));
+            for (auto &i : vector_of_unique_patterns) {
+                if (i != vector_of_kmers.end()->pattern) {
+                    vector_of_unique_patterns.push_back(vector_of_kmers.end()->pattern);
+                }
+            }
         }
     }
     stream.close();
 
     //iterate with iterator on vectorOfKmer
-    outstream << "ps\t" ;
-    for (const Kmer &i : vectorOfKmers) {
-        outstream << i.name << "\t" ;
+    outstream << "ps\t";
+    outstream_unique << "ps\t";
+    for (const Kmer &i : vector_of_kmers) {
+        outstream << i.name << "\t";
+        outstream_unique << i.name << "\t";
     }
     outstream << "\n" ;
-    for (int i = 0; i < vectorOfKmers.at(1).pattern.size(); ++i) { //gets the number of lines that will be written, which corresponds to the number of 0/1 in the vector pattern of the structures
+    // gets the number of lines that will be written, which corresponds to the number of 0/1 in the vector pattern of the structures
+    for (int i = 0; i < vector_of_kmers.at(1).pattern.size(); ++i) {
         outstream << i << "\t" ;
         //go through the ieme values of each vector
-        for (auto j: vectorOfKmers) {
+        for (auto j: vector_of_kmers) {
             outstream << j.pattern.at(i) << "\t";
         }
         outstream << "\n" ;
     }
-
     outstream.close();
+
+    // gets the number of lines that will be written, which corresponds to the number of 0/1 in the pattern of each vector
+    // major diffference with above is that we iterate directly on the pattern vectors instead of the structures containing them
+    for (int i = 0; i < vector_of_unique_patterns.at(1).size(); ++i) {
+        outstream_unique << i << "\t" ;
+        //go through the ieme values of each vector
+        for (auto j: vector_of_unique_patterns) {
+            outstream_unique << j.at(i) << "\t";
+        }
+        outstream_unique << "\n" ;
+    }
 
     //finishing measuring time
     auto t2 = std::chrono::high_resolution_clock::now();
